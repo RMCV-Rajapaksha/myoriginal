a6bdd41083dbb23931fb128884f157eaad46bb7b
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-09-21 10:17:31 +0530

    Refactor utility function, update metadata, and add FloatingNav component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-
+import { FaLocationArrow } from "react-icons/fa6";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
+        <FloatingNav navItems={[
+          {
+            name: "Home",
+            link: "/",
+            icon: <FaLocationArrow />,
+          }
+        ]}/>
         <Hero/>
       </div>
     </main>
   );
 }
-
